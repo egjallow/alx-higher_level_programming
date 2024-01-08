7f23d866b7ba314e0f8b411dd4b76a8c2c6107f5
egjallow10 <egjallow10@gmail.com>
2024-01-08 21:20:28 +0000

    print numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):
-    if i < 10:
-        print("{}{}".format(0, i), end=", ")
-    elif i != 99:
-        print("{}".format(i), sep=" ", end=", ")
+for i in range(00, 100):
+    if i != 99:
+        print("{:02}".format(i), end=", ")
     elif i == 99:
         print("{}".format(i))
