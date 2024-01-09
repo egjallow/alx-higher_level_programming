4362043be17e9d810e179da6c0419808e5357df3
egjallow10 <egjallow10@gmail.com>
2024-01-09 12:24:06 +0000

    print upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for char in str:
+        ascii_value = ord(char)
+        if 97 <= ascii_value <= 122:
+            uppercase_char = chr(ascii_value - 32)
+        else:
+            uppercase_char = char
+        print("{}".format(uppercase_char), end='')
+
+    print()
