b827d428a1d0920c40eba0c28bbf392344c29ec4
egjallow10 <egjallow10@gmail.com>
2024-01-09 09:18:23 +0000

    final changes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(10):
-    for j in range(i+1, 10):
-        if i < 8 or (i == 8 and j == 9):
-            print("{}{}".format(i, j), end=', ')
-        else:
-            print("{}{}".format(i, j))
+    for j in range(i + 1, 10):
+        print(
+            "{}{}".format(i, j),
+            end=", " if int(str(i) + str(j)) < 89 else "\n"
+        )
