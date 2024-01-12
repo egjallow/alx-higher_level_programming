11353d3a14865c08a3e723819920df31fe923021
egjallow <egjallow7@gmial.com>
2024-01-12 15:16:45 +0000

    fix pycodestyle

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,9 +2,10 @@
 
 
 def print_list_integer(my_list=[]):
-        if len(my_list) > 0:
-            for i in my_list:
-                print("{:d}".format(i))
+    if len(my_list) > 0:
+        for i in my_list:
+            print("{:d}".format(i))
+
+
 if __name__ == "__main__":
     print_list_integer(my_list=[])
-
