5cfe4d36fead2e8b229da74d3b843b3c4b696857
egjallow <egjallow7@gmial.com>
2024-01-25 13:41:05 +0000

    fix pycode style

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,14 +5,15 @@ This module defines a Square class && initialise its size and check
 for it's attribute
 """
 
+
 class Square:
     """Square implementation
     """
+
     def __init__(self, size=0):
         """initialise"""
 
-
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
