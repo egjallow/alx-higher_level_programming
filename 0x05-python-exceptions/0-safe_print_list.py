26566057b8f303c4cf3743e4aa59847830595cc3
egjallow <egjallow7@gmail.com>
2024-04-02 08:09:11 -0400

    changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end=' ')
             count += 1
-    except IndexError:
+    except Exception as error:
         pass
-    finally:
-        print()
+    print('')
     return count
 
