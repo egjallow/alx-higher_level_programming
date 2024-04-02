7334c9213931c9a05d8f882d039ed926b9ab83a1
egjallow <egjallow7@gmail.com>
2024-04-02 08:02:36 -0400

    make changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3 
 
-
 def safe_print_list(my_list=[], x=0):
-    str_len = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end='')
-            str_len += 1
-        except Exception as error:
-            break
-    print('')
-    return str_len
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=' ')
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return count
 
