2db2f92b7f8df60764f004d73746410280de9d1c
egjallow <egjallow7@gmail.com>
2024-04-03 11:49:14 -0400

    removed space

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,8 +10,6 @@ class Square:
     """
     def __init__(self, size=0):
         """initialisation"""
-
-
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
@@ -20,7 +18,4 @@ class Square:
 
     def area(self):
         """Area of the square"""
-
-
         return (self.__size ** 2)
-
