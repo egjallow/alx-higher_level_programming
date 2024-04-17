3b1e3ca52a3d3476563622a97e7ccc0398813400
egjallow <egjallow7@gmail.com>
2024-04-17 19:50:46 -0400

    fix lint error and others

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""" module `2-is_same_class` provides `is_same_class` function """
+
+
 def is_same_class(obj, a_class):
-    """
-    returns True if object is same as class, False otherwise
-    """
+    """ tests if `obj` is exactly an instance of `a_class` """
     return type(obj) == a_class
