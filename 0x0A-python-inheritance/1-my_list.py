2a77e539fee410ccdd669552daf29113f2bc3963
egjallo <ejallow@insistglobal.com>
2024-04-19 03:46:00 -0600

    fix errors

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,10 +8,6 @@ class MyList(list):
     """class that inherits a list"""
 
     def print_sorted(self):
-        """
-        prints list in ascending sort
-        """
-        sort_list = super().copy()
-        sort_list = sorted(sort_list)
-        print(sort_list)
-        
+        """Sorted list function"""
+        sorted_list = sorted(self)
+        print(sorted_list)
