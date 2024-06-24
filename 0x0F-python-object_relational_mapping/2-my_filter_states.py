72739313ed756c0362cd90ce0a6c999aabe1790e
egjallow <egjallow7@gmail.com>
2024-06-24 14:44:05 +0000

    filter by name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Takes in an argument and displays all values in the `states`
+table of `hbtn_0e_0_usa` where `name` matches the argument.
+"""
 
 if __name__ == '__main__':
     from sys import argv
@@ -6,18 +10,20 @@ if __name__ == '__main__':
 
     name = argv[4]
 
-    conn = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
-    c = conn.cursor()
-    c.execute(("SELECT * FROM states "
-               "WHERE name='{}' "
-               "ORDER BY id".format(name)))
-
-    rows = c.fetchall()
-
-    for row in rows:
-        print(row)
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cursor = db.cursor()
+    cursor.execute(("SELECT * FROM states "
+                    "WHERE name='{}' "
+                    "ORDER BY id".format(name)))
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == name:
+            print(state)
+    cursor.close()
+    db.close()
