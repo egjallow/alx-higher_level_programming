b12066b10be051f730c65a6a09251ab2c7bb08bf
egjallow <egjallow7@gmail.com>
2024-06-24 12:52:54 +0000

    fix the pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Lits all state from the databse hbtn_0e_0_usa"""
 
-if __name__== '__main__':
+if __name__ == '__main__':
     from sys import argv
     import MySQLdb
 
-
-    conn = MySQLdb.connect(host='localhost', port=3306,user=argv[1], passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
     c = conn.cursor()
     c.execute('SELECT * FROM states ORDER BY id ASC')
     rows = c.fetchall()
