b65abee7c4ced609de6676aefd91885ca22ab021
egjallow <egjallow7@gmail.com>
2024-06-24 14:16:03 +0000

    ists all states with a name

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,8 +4,6 @@
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb
-
-    # Connect to the database
     conn = MySQLdb.connect(
         host='localhost',
         port=3306,
@@ -13,19 +11,10 @@ if __name__ == '__main__':
         passwd=argv[2],
         db=argv[3])
 
-    # Create a cursor object
     c = conn.cursor()
-
-    # Execute the SQL query to find states with names starting with 'N'
     c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all the results
     rows = c.fetchall()
-
-    # Print the results
     for row in rows:
         print(row)
-
-    # Close the cursor and connection
     c.close()
     conn.close()
