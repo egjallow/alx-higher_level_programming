e7ddb34c48d45eb2d504acb96d8b0b75bc8d3eb2
egjallow <egjallow7@gmail.com>
2024-06-26 14:51:54 +0000

    fixed

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     target_state = (argv[4], )
 
     db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
     cursor = db.cursor()
     cursor.execute(("SELECT cities.name "
                     "FROM states JOIN cities "
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,5 +9,11 @@ from sqlalchemy import create_engine
 Base = new_declarative_base()
 if __name__ == "__main__":
     from urllib.parse import quote_plus
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], quote_plus(sys.argv[2]), sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            quote_plus(
+                sys.argv[2]),
+            sys.argv[3]),
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
