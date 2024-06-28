faff80597556b66a8c0dfce20eb2942e5f815a48
egjallow <egjallow7@gmail.com>
2024-06-28 13:24:47 +0000

    fetch states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,20 +4,31 @@ The `State` class inherits from `Base`, an SQLAlchemy declarative base.
 Instances of `State` will be a MySQL database.
 """
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sys import argv
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+from model_state import Base, State
+from urllib.parse import quote_plus
 
-Base = declarative_base()
+if __name__ == '__main__':
 
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    host = 'localhost'
+    port = 3306
 
-class State(Base):
-    """
-    This class is linked to the states table.
-    Attributes:
-        id: Integer, required, primary key
-        name: String, required
-    """
-    __tablename__ = 'states'
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{quote_plus(password)}@{host}:{port}/{db_name}')
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
+    Base = declarative_base()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
