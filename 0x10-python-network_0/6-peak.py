e6c500b055da5c9d741960ceed590e3a4c548a3d
egjallow <egjallow7@gmail.com>
2024-07-11 08:31:40 +0000

    changes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" a function that finds a peak in a list of unsorted integers """
-
+"""A function that finds a peak in a list of unsorted integers."""
 
 def find_peak(list_of_integers):
-    """ find the peak number with the shortest algorithm"""
+    """Find the peak number with the shortest algorithm."""
+    if not list_of_integers:
+        return None
+    return max(list_of_integers)
 
-    l = list_of_integers
-    return max(l) if l else None
