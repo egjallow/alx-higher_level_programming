d76ed2406569ecaf1c9be3c3c67096b3839d055d
egjallow <egjallow7@gmail.com>
2024-07-11 08:00:19 +0000

    change the algo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,23 +4,5 @@
 
 def find_peak(list_of_integers):
     """ find the peak number with the shortest algorithm"""
-    if list_of_integers == []:
-        return None
-
-    size = len(list_of_integers)
-    if size == 0:
-        return (None)
-    elif size == 1:
-        return (list_of_integers[0])
-    elif size == 2:
-        return max(list_of_integers)
-
-    mid = int(size/2)
-    peak = list_of_integers[mid]
-    mylist = list_of_integers
-    if peak > mylist[mid - 1] and peak > mylist[mid + 1]:
-        return peak
-    elif peak < mylist[mid - 1]:
-        return find_peak(mylist[:mid])
-    else:
-        return find_peak(mylist[mid + 1:])
+    l = list_of_integers
+    return max(l) if l else None
